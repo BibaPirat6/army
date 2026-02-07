958f1a7d7673547fec876db6662e63a2c901a7dd
Ваше Имя <@email.com>
2026-02-07 11:14:39 +0300

    start fix bugs

--- resources/views/admin/org/commissariats/show.blade.php
+++ resources/views/admin/org/commissariats/show.blade.php
@@ -69,6 +69,37 @@
                         <span
                             class="text-[#060606]">{{ \Carbon\Carbon::parse($commissariat['updated_at'])->format('d.m.Y H:i') }}</span>
                     </div>
+
+
+
+
+                    {{-- можно бы тут сделать кнопки + сделать back_url --}}
+                    <div>
+                        <a href="{{ route('commissariats.edit', $commissariat->id) }}"
+                            class="inline-flex items-center px-4 py-2 bg-[#A60644] text-white text-sm font-medium rounded-lg hover:bg-[#A60644]/80 transition-colors duration-200 shadow-sm hover:shadow-md">
+                            <svg class="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z">
+                                </path>
+                            </svg>
+                            Редактировать
+                        </a>
+                        <form action="{{ route('commissariats.delete', $commissariat->id) }}" method="POST"
+                            class="inline-block mt-0.5"
+                            onsubmit="return confirm('Вы уверены, что хотите удалить комиссариат \"{{ $commissariat->name }}\"?');">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit"
+                                class="inline-flex items-center px-4 py-2 bg-[#060606] text-white text-sm font-medium rounded-lg hover:bg-[#060606]/80 transition-colors duration-200 shadow-sm hover:shadow-md">
+                                <svg class="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16">
+                                    </path>
+                                </svg>
+                                Удалить
+                            </button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
